b48b46854ef24e961023036399ce63e54a53867e
Ken Kiers <knkiers@taylor.edu>
2019-07-24 10:08:59 -0400

    embed course info in hidden panel in table

--- planner/api_views.py
+++ planner/api_views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 
 from .models import *
+from .helper_functions import *
 
 from django.shortcuts import render
 
@@ -11,8 +12,109 @@ from django.http import JsonResponse
 
 @login_required
 def banner_comparison_data(request):
+
+    department_id = request.GET.get('departmentId')
+    year_id = request.GET.get('yearId')
+    print('dept: ', department_id)
+    print('year: ', year_id)
+
+    courses = [
+        Course.objects.filter(title='Modern Physics')[0],
+        Course.objects.filter(title='University Physics II')[0],
+        Course.objects.filter(title='Engineering Thermodynamics')[0],
+        Course.objects.filter(title='Analytical Mechanics')[0]
+    ]
+    
+    course_data = []
+    for course in courses:
+        num_offerings = len(course.offerings.all())
+        
+        co1 = course.offerings.all()[num_offerings-1]
+        co2 = course.offerings.all()[num_offerings-2]
+        co1meeting_times_list, co1room_list = class_time_and_room_summary(co1.scheduled_classes.all())
+        co2meeting_times_list, co2room_list = class_time_and_room_summary(co2.scheduled_classes.all())
+        instructors1 = [instr.instructor.first_name+' '+instr.instructor.last_name for instr in co1.offering_instructors.all()]
+        instructors2 = [instr.instructor.first_name+' '+instr.instructor.last_name for instr in co2.offering_instructors.all()]
+        course_data.append({
+            "semester": 'Fall',
+            "course_id": co1.course.id,
+            "course": co1.course.subject.abbrev+' '+co1.course.number,
+            "course_title": co1.course.title,
+            "banner": { 
+                "course_offering_id": co1.id,
+                "meeting_times": co1meeting_times_list,
+                "rooms": co1room_list,
+                "instructors": instructors1
+            },
+            "ichair": { 
+                "course_offering_id": co2.id,
+                "meeting_times": co2meeting_times_list,
+                "rooms": co2room_list,
+                "instructors": instructors2
+            },
+            "has_banner": True,
+            "has_ichair": True,
+            "linked": True,
+            "delta": None,
+            "needs_work": True,
+            "crn": '12354'
+        })
+
+    course = Course.objects.filter(title='Quantum Mechanics I')[0]
+    num_offerings = len(course.offerings.all())
+    co = course.offerings.all()[num_offerings-1]
+    meeting_times_list, room_list = class_time_and_room_summary(co1.scheduled_classes.all())
+    instructors = [instr.instructor.first_name+' '+instr.instructor.last_name for instr in co.offering_instructors.all()]
+    course_data.append({
+        "semester": "J-term",
+        "course_id": co.course.id,
+        "course": co.course.subject.abbrev+' '+co.course.number,
+        "course_title": co.course.title,
+        "banner": { 
+                "course_offering_id": co.id,
+                "meeting_times": meeting_times_list,
+                "rooms": room_list,
+                "instructors": instructors
+            },
+        "ichair": None,
+        "has_banner": True,
+        "has_ichair": False,
+        "linked": False,
+        "delta": None,
+        "needs_work": False,
+        "crn": '52846'
+    })
+
+    course = Course.objects.filter(title='Quantum Mechanics II - SP')[0]
+    num_offerings = len(course.offerings.all())
+    co = course.offerings.all()[num_offerings-1]
+    meeting_times_list, room_list = class_time_and_room_summary(co1.scheduled_classes.all())
+    instructors = [instr.instructor.first_name+' '+instr.instructor.last_name for instr in co.offering_instructors.all()]
+    course_data.append({
+        "semester": 'Spring',
+        "course_id": co.course.id,
+        "course": co.course.subject.abbrev+' '+co.course.number,
+        "course_title": co.course.title,
+        "ichair": { 
+                "course_offering_id": co.id,
+                "meeting_times": meeting_times_list,
+                "rooms": room_list,
+                "instructors": instructors
+            },
+        "banner": None,
+        "has_banner": False,
+        "has_ichair": True,
+        "linked": False,
+        "delta": None,
+        "needs_work": True,
+        "crn": None
+    })
+
+    print(course_data)
+
     data = {
-        "message": "hello!"
+        "message": "hello!",
+        "course_data": course_data
     }
     return JsonResponse(data)
 
--- planner/views.py
+++ planner/views.py
@@ -14,6 +14,7 @@ from django.utils.functional import curry
 
 from .models import *
 from .forms import *
+from .helper_functions import *
 
 import json
 import csv
@@ -1163,57 +1164,6 @@ def class_time_summary(scheduled_classes):
 #       ===> if this is done, need to edit the template a bit and also the "---" case, to make it
 #            a simple string instead of a list
     return class_times_list
-
-def class_time_and_room_summary(scheduled_classes):
-# Returns a class time summary list and an accompanying room list, such as ['MWF 9-9:50','T 10-10:50'] and ['NS 210', 'ESC 141']
-# scheduled_classes is assumed to be a list of ScheduledClass objects with at least one element
-
-    day_list = ['M','T','W','R','F']
-    time_dict = dict()
-    room_dict = dict()
-    day_dict = dict()
-    schedule_list = []
-    for sc in scheduled_classes:
-        time_string=start_end_time_string(sc.begin_at.hour, sc.begin_at.minute, sc.end_at.hour, sc.end_at.minute)
-        if sc.room != None:
-            room = sc.room.building.abbrev+' '+sc.room.number
-        else:
-            room = '---'
-        schedule_list.append([sc.day, time_string, room])
-        day_dict[time_string+room]=''
-        room_dict[time_string+room] = room
-        time_dict[time_string+room] = time_string
-
-    schedule_sorted = sorted(schedule_list, key=lambda row: (row[0], row[1]))
-
-    for item in schedule_sorted:
-        day_dict[item[1]+item[2]]=day_dict[item[1]+item[2]]+day_list[item[0]]
-
-    class_times_list = []
-    room_list = []
-    for key in list(day_dict.keys()):
-        class_times_list.append(day_dict[key]+' '+time_dict[key])
-        room_list.append(room_dict[key])
-
-    return class_times_list, room_list
-
-
-
-
-def start_end_time_string(start_hour,start_minute,end_hour,end_minute):
-# given starting and ending data, returns a string such as '9-9:50' or '9:15-10:30'
-
-    time = str(start_hour)
-    if 0<start_minute < 9:
-        time = time+':0'+str(start_minute)
-    elif start_minute>9:
-        time = time+':'+str(start_minute)
-    time = time+'-'+str(end_hour)
-    if 0<end_minute < 9:
-        time = time+':0'+str(end_minute)
-    elif end_minute > 9:
-        time = time+':'+str(end_minute)
-    return time
 
 
 @login_required
