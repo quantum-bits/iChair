7be751b25bea42c979931335c450fd9295eecdb5
iChair Install User <ichair@ichair.cse.taylor.edu>
2019-09-25 16:44:36 -0400

    production configuration changes

--- four_year_plan/settings.py
+++ four_year_plan/settings.py
@@ -37,7 +37,7 @@ else:
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'ichair.cse.taylor.edu']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
