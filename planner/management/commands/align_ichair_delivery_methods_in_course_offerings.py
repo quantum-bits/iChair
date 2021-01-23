1cc5ea5b9458a11537e8f7ca286ba8f16d7306ac
Ken Kiers <knkiers@taylor.edu>
2021-01-23 08:44:57 -0500

    add more reporting to delivery methods command

--- planner/management/commands/align_ichair_delivery_methods_in_course_offerings.py
+++ planner/management/commands/align_ichair_delivery_methods_in_course_offerings.py
@@ -7,7 +7,7 @@ from banner.models import SemesterCodeToImport as BannerSemesterCodeToImport
 
 class Command(BaseCommand):
     # https://stackoverflow.com/questions/30230490/django-custom-command-error-unrecognized-arguments
-    help = "Copies delivery methods from the banner database to the iChair database for all iChair course offerings that have a CRN.  This command overwrites the delivery methods property and should not generally be used."
+    help = "Copies delivery methods from the banner database to the iChair database for all iChair course offerings that have a CRN."
 
     def handle(self, *args, **options):
         num_unmatched = 0
@@ -16,11 +16,16 @@ class Command(BaseCommand):
         num_delivery_method_errors = 0
         num_course_offerings_with_crn = 0
         num_already_had_delivery_method = 0
+        num_did_not_have_delivery_method = 0
         num_no_stored_crn_now_updated_delivery_method = 0
+        num_no_crn_and_no_match_found_in_banner = 0
+        num_course_offerings = 0
         for banner_semester in BannerSemesterCodeToImport.objects.all():
             for co in CourseOffering.objects.filter(semester__banner_code = banner_semester.term_code):
                 # only add a delivery method if the course offering doesn't currently have one
+                num_course_offerings += 1
                 if co.delivery_method is None:
+                    num_did_not_have_delivery_method += 1
                     if co.crn is not None:
                         num_course_offerings_with_crn += 1
                         banner_course_offerings = BannerCourseOffering.objects.filter(Q(crn = co.crn) & Q(term_code = co.semester.banner_code))
@@ -69,14 +74,17 @@ class Command(BaseCommand):
                                 num_delivery_method_errors += 1
                             print("banner delivery method: ", banner_delivery_method)
                             print("updated iChair delivery method: ", co.delivery_method)
-
-
+                        else:
+                            num_no_crn_and_no_match_found_in_banner += 1
                 else:
                     print('already has a delivery method: ', co)
                     num_already_had_delivery_method += 1
-
+        
+        print('number of iChair course offerings: ', num_course_offerings)
         print('number of course offerings that already had a delivery method: ', num_already_had_delivery_method)
+        print('number of course offerings that did not already have a delivery method:', num_did_not_have_delivery_method)
         print('number that did not have a stored crn but a match was found and delivery method was updated: ', num_no_stored_crn_now_updated_delivery_method)
+        print('number that did not have a stored crn and no match was found: ', num_no_crn_and_no_match_found_in_banner)
         print('number of unmatched course offerings: ',num_unmatched)
         print('number of multiple matches: ', num_multiple_matches)
         print('number of exact matches: ', num_exact_matches)
