31cbfdd8152cd68372d562b0b7257425521ae60b
Ken Kiers <knkiers@taylor.edu>
2021-03-05 11:01:03 -0500

    fix bug related to including extra-departmental course sections in schedule edits

--- banner/models.py
+++ banner/models.py
@@ -171,27 +171,30 @@ class CourseOffering(StampedModel):
             return cls.objects.filter(Q(course__subject__abbrev=subject.abbrev) & Q(term_code=term_code))
         else:
             course_offerings = []
+            #print('extra departmental courses: ', extra_departmental_courses)
             for course in extra_departmental_courses:
-                for ii in range(1,len(course["number"])+1):
-                    # iChair course numbers might be something like '311L'; this needs to match '311' in Banner;
-                    # here I am considering the beginnings of the iChair course numbers and looking for an exact match
-                    # with Banner: '3', '31', '311' and '311L' would all be searched
-                    course_num = course["number"][:ii]
-                    #print(course)
-                    #print('searching course number....', course_num)
-                    for co in cls.objects.filter(
-                        Q(course__subject__abbrev=subject.abbrev) & 
-                        Q(term_code=term_code) & 
-                        Q(course__credit_hours = course["credit_hours"]) & 
-                        Q(course__number = course_num)):
-                        print('>>>>>found the following: ', co, ' ', co.id)
-                        list_contains_co = False
-                        for collected_co in course_offerings:
-                            if co.id == collected_co.id:
-                                list_contains_co = True
-                                print('Yikes!  Almost copied this one twice!!!')
-                        if not list_contains_co:
-                            course_offerings.append(co)
+                if subject.abbrev == course['subject_abbrev']:
+                    for ii in range(1,len(course["number"])+1):
+                        # iChair course numbers might be something like '311L'; this needs to match '311' in Banner;
+                        # here I am considering the beginnings of the iChair course numbers and looking for an exact match
+                        # with Banner: '3', '31', '311' and '311L' would all be searched
+                        course_num = course["number"][:ii]
+                        #print(course)
+                        #print('searching course number....', course_num)
+                        #print('subject abbreviation: ', subject.abbrev)
+                        for co in cls.objects.filter(
+                            Q(course__subject__abbrev=subject.abbrev) & 
+                            Q(term_code=term_code) & 
+                            Q(course__credit_hours = course["credit_hours"]) & 
+                            Q(course__number = course_num)):
+                            print('>>>>>found the following: ', co, ' ', co.id)
+                            list_contains_co = False
+                            for collected_co in course_offerings:
+                                if co.id == collected_co.id:
+                                    list_contains_co = True
+                                    print('Yikes!  Almost copied this one twice!!!')
+                            if not list_contains_co:
+                                course_offerings.append(co)
             #print('>>>>>>>extra-departmental course offerings found: ', course_offerings)
             return course_offerings
 
